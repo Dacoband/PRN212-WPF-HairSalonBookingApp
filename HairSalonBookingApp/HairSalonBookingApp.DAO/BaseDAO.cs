1055a1911729bfd219d95f7c7c804a3b49c848e8
kopami <khoipham2310@gmail.com>
2024-10-21 22:29:15 +0700

    [KhoiPM][Dev]: Develop Base DAO

---
+++ HairSalonBookingApp/HairSalonBookingApp.DAO/BaseDAO.cs
@@ -0,0 +1,120 @@
+﻿using HairSalonBookingApp.DAO.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace HairSalonBookingApp.DAO
+{
+    public class BaseDAO<T> where T : class
+    {
+        protected ApplicationDbContext? _context;
+        internal DbSet<T>? _dbSet;
+
+        public BaseDAO(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public bool Add(T entity)
+        {
+            bool result = false;
+            try
+            {
+                if (entity != null)
+                {
+                    _dbSet.Add(entity);
+                    _context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+
+        public bool Update(T entity)
+        {
+            bool result = false;
+            try
+            {
+                if (entity != null)
+                {
+                    _dbSet.Update(entity);
+                    _context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+
+        public bool Delete(T entity)
+        {
+            bool result = false;
+            try
+            {
+                if (entity != null)
+                {
+                    _dbSet.Remove(entity);
+                    _context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+
+        public T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = _dbSet;
+            }
+            else
+            {
+                query = _dbSet.AsNoTracking();
+                //no tracking : Database will not be updated if the Update method is not called before the Save method                
+            }
+            query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefault();
+
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.ToList();
+        }
+    }
+}
---
+++ HairSalonBookingApp/HairSalonBookingApp.DAO/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonBookingApp.DAO.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private string? GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            return config.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString() ?? string.Empty);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
