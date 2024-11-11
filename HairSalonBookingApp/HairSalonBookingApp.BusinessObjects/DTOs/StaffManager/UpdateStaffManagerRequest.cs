659399c1347bdf1828636b9db763f081f1db35e1
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-11-11 17:34:16 +0700

    Branch Service CRUD Service

---
+++ HairSalonBookingApp/HairSalonBookingApp.BusinessObjects/DTOs/StaffManager/UpdateStaffManagerRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonBookingApp.BusinessObjects.DTOs.StaffManager
+{
+    public class UpdateStaffManagerRequest
+    {
+        public Guid? BranchID { get; set; }
+        public string? StaffManagerName { get; set; } 
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; } 
+        public string? Address { get; set; }
+        public string? AvatarImage { get; set; }
+    }
+}
