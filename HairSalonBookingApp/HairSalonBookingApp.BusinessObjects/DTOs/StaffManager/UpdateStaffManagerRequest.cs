62aae6bcffded1d385573ba6db31453e79bc6f8c
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-11-11 20:30:11 +0700

    StaffManager CRUD in layer Services

--- HairSalonBookingApp/HairSalonBookingApp.BusinessObjects/DTOs/StaffManager/UpdateStaffManagerRequest.cs
+++ HairSalonBookingApp/HairSalonBookingApp.BusinessObjects/DTOs/StaffManager/UpdateStaffManagerRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,6 @@ namespace HairSalonBookingApp.BusinessObjects.DTOs.StaffManager
         public DateTime DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; } 
         public string? Address { get; set; }
-        public string? AvatarImage { get; set; }
+        public IFormFile? AvatarImage { get; set; }
     }
 }
--- HairSalonBookingApp/HairSalonBookingApp.Service/Interface/IStaffManagerService.cs
+++ HairSalonBookingApp/HairSalonBookingApp.Service/Interface/IStaffManagerService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HairSalonBookingApp.BusinessObjects.DTOs.StaffManager;
+using HairSalonBookingApp.BusinessObjects.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,10 @@ namespace HairSalonBookingApp.Services.Interface
 {
     public interface IStaffManagerService
     {
+        Task<ActionResult<List<StaffManager>>> GetAllStaffManager();
+        Task<ActionResult<StaffManager>> GetStaffManagerById(Guid managerId);
+        Task<ActionResult> CreateStaffManager(CreateStaffManagerRequest createStaffManagerRequest);
+        Task<(bool, string)> UpdateStaffManager(UpdateStaffManagerRequest updateStaffManagerRequest);
+        Task<bool> DeleteStaffManager(Guid managerId);
     }
 }
