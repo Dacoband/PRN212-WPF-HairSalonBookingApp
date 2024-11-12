a719545cf7d4cfc21413f4cdbede4e382c61b0bc
thucnguyen1234 <hantruong3009@gmail.com>
2024-11-12 22:14:09 +0700

    Add Page Stylist

--- HairSalonBookingApp/HairSalonBookingApp/Program.cs
+++ HairSalonBookingApp/HairSalonBookingApp/Program.cs
@@ -38,6 +38,8 @@ builder.Services.AddScoped<IServiceService, ServiceService>();
 builder.Services.AddScoped<IFirebaseService, FirebaseService>();
 builder.Services.AddScoped<IBranchService, BranchService>();
 builder.Services.AddScoped<IStaffManagerService, StaffManagerService>();
+builder.Services.AddScoped<IStaffStylistService, StaffStylistService>();
+builder.Services.AddScoped<IStylistService, StylistService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddSession();
