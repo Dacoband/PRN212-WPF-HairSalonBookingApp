949e5f93a69e9b70b75eb3ca1794f61835b5a4c7
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-11-12 16:41:49 +0700

    SMTP Email Service in layer Service

--- HairSalonBookingApp/HairSalonBookingApp/Program.cs
+++ HairSalonBookingApp/HairSalonBookingApp/Program.cs
@@ -37,6 +37,13 @@ builder.Services.AddRazorPages();
 builder.Services.AddScoped<IServiceService, ServiceService>();
 builder.Services.AddScoped<IFirebaseService, FirebaseService>();
 builder.Services.AddScoped<IBranchService, BranchService>();
+builder.Services.AddScoped<IStaffManagerService, StaffManagerService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IStylistService, StylistService>();
+builder.Services.AddScoped<IFeedbackService, FeedbackService>();
+builder.Services.AddScoped<IStaffStylistService, StaffStylistService>();
 builder.Services.AddSession();
 
 
