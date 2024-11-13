00a7bed9bda75f3172dac5a7093c51c25d22c7fc
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-11-13 13:57:03 +0700

    UI Time

---
+++ HairSalonBookingApp/HairSalonBookingApp/Pages/Time/SelectTimePage.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+
+namespace HairSalonBookingApp.Pages.Time
+{
+    public class SelectTimePageModel : PageModel
+    {
+        public DateTime selectedDate { get; set; } = DateTime.Today;
+        public DateTime? selectedTime { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        [HttpPost]
+        public JsonResult OnPostUpdateDate([FromBody] DateTime date)
+        {
+            selectedDate = date;
+            selectedTime = null; // Reset thời gian đã chọn khi thay đổi ngày
+            return new JsonResult(new { success = true });
+        }
+
+        public void SelectTime(DateTime time)
+        {
+            selectedTime = time;
+        }
+    }
+}
